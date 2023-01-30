4a40d7e597482bf90a801f1a656fbdce89dff488
pedrinchixx <120227882+pedrinchixx@users.noreply.github.com>
2023-01-30 17:37:52 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,168 @@
+import {productos} from "./productos.js"
+import Producto from "./producto.js";
+
+
+function cargarTabla(listaProductos){
+    let cuerpoTabla = document.querySelector(".section_mantenedor_productos tbody");
+    cuerpoTabla.innerHTML = "";
+
+    let acumuladorFilas = "";
+    listaProductos.forEach(producto => {
+        acumuladorFilas += `
+                <tr>
+                    <th scope="row">${producto.id}</th>
+                    <td>${producto.nombre}</td>
+                    <td>${producto.descripcion}</td>
+                    <td>${producto.precio}</td>
+                    <td>${producto.stock}</td>
+                </tr>
+        `
+    });
+    cuerpoTabla.innerHTML = acumuladorFilas;
+
+}
+
+function buscarProducto(id){
+    let producto = new Producto(id);
+    return producto.getProduct();
+}
+
+crud_form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+})
+
+
+//funcion que capture evento del input crud_id
+
+let inputId = document.getElementById("crud_id");
+inputId.addEventListener("change", (event) =>{
+    event.preventDefault();
+    let id =  inputId.value;
+    let producto = buscarProducto(id);
+    if(producto){
+        crud_nombre.value = producto.nombre;
+        crud_descripcion.value = producto.descripcion;
+        crud_precio.value = producto.precio;
+        crud_stock.value = producto.stock;
+    }else{
+        crud_nombre.value = "";
+        crud_descripcion.value = "";
+        crud_precio.value = 0;
+        crud_stock.value = 0;
+
+    }
+})
+
+//AGREGAR PRODUCTOS
+document.getElementById("btn-agregar").addEventListener("click", (event)=> {
+    event.preventDefault();
+    let id = crud_id.value;
+    let nombre = crud_nombre.value;
+    let descripcion = crud_descripcion.value;
+    let precio = crud_precio.value;
+    let stock = crud_stock.value;
+    
+    let nuevoProducto = new Producto(id, nombre, descripcion, precio, stock);
+    if(nuevoProducto.getProduct()){
+        alert("Ya existe un producto con dicho ID.")
+    }else{
+        nuevoProducto.addProduct();
+
+
+
+        Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Producto Fue Agregado.!",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+
+
+
+
+        cargarTabla(nuevoProducto.getProducts());
+    } 
+})
+
+//ELIMINAR PRODUCTOS
+document.getElementById("btn-eliminar").addEventListener("click", (event)=> {
+    event.preventDefault();
+    let id = crud_id.value;
+    
+    let producto = new Producto(id);
+
+
+    if(producto.getProduct()){
+        let respuesta = confirm("Está seguro que quiere eliminar el producto con ID: " + producto.id);
+        
+        Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Producto Eliminado Correctamente.!",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        
+        
+        
+        if(respuesta){
+            producto.deleteProduct();
+
+
+        cargarTabla(producto.getProducts());
+        }
+        
+
+    }else{
+        alert("El producto que intenta eliminar no existe en la BD.")
+    }
+    
+})
+
+//MODIFICAR PRODUCTOS
+document.getElementById("btn-modificar").addEventListener("click", (event)=> {
+    event.preventDefault();
+    let id = crud_id.value;
+    let nombre = crud_nombre.value;
+    let descripcion = crud_descripcion.value;
+    let precio = crud_precio.value;
+    let stock = crud_stock.value;
+    
+    let producto= new Producto(id, nombre, descripcion, precio, stock);
+    if(producto.getProduct()){
+        producto.updateProduct();
+
+
+        Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Producto Fue Actualizado.!",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        
+
+
+
+        cargarTabla(producto.getProducts());
+    }else{
+        alert("El producto que intenta actualizar no existe en la BD.")
+    } 
+})
+
+function main(){
+    //let productosStorage = JSON.parse(localStorage.getItem("productos"));
+    let productosStorage;
+    if(JSON.parse(localStorage.getItem("inventario"))){
+         productosStorage = JSON.parse(localStorage.getItem("inventario"));
+    }else{
+        productosStorage = productos;
+        localStorage.setItem("inventario", JSON.stringify(productosStorage))
+    }
+    cargarTabla(productosStorage);
+    }
+    
+
+
+main();
